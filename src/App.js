87cad4e17ec85ec74a492c01a7cc5691fd6be1fe
andressa-raraujo <andressa.raraujo@outlook.com>
2021-10-27 19:53:35 -0300

    Conclusão

--- src/App.js
+++ src/App.js
@@ -6,11 +6,27 @@ import ListaDeNotas from "./components/ListaDeNotas";
 import "./assets/App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notas: [],
+    };
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayDeNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayDeNotas,
+    };
+    this.setState(novoEstado);
+  }
+
   render() {
     return (
       <div className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
       </div>
     );
   }
