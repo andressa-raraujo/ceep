725545866154331eab8b47368e06e07dcb2dd394
andressa-raraujo <andressa.raraujo@outlook.com>
2021-10-26 23:51:49 -0300

    Adicionando estilo a lista de notas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import FormularioCadastro from "./components/formularioCadastro";
-
-import ListaDeNotas from "./components/listaDeNotas";
+import ListaDeNotas from "./components/ListaDeNotas/listaDeNotas";
 
 class App extends Component {
   render() {
