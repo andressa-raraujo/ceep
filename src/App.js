2281926e87e25d68671168b124515b03cb4779fb
andressa-raraujo <andressa.raraujo@outlook.com>
2021-10-27 00:14:17 -0300

    Adicionando estilo ao formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
-import FormularioCadastro from "./components/formularioCadastro";
+import FormularioCadastro from "./components/FormularioCadastro/formularioCadastro";
+
 import ListaDeNotas from "./components/ListaDeNotas/listaDeNotas";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="conteudo">
         <FormularioCadastro />
         <ListaDeNotas />
       </div>
