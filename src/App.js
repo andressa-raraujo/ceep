ab021492990c7a1b248999539089fccfd3321655
andressa-raraujo <andressa.raraujo@outlook.com>
2021-10-27 00:32:04 -0300

    Adicionando arquivo de index aos componentes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import FormularioCadastro from "./components/FormularioCadastro/formularioCadastro";
+import FormularioCadastro from "./components/FormularioCadastro";
 
-import ListaDeNotas from "./components/ListaDeNotas/listaDeNotas";
+import ListaDeNotas from "./components/ListaDeNotas";
 
 import "./assets/App.css";
 
--- src/components/ListaDeNotas/listaDeNotas.jsx
+++ src/components/ListaDeNotas/listaDeNotas.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import CardNota from "../CardNota/CardNota";
+import CardNota from "../CardNota";
 import "./listaDeNotas.css";
 
 class ListaDeNotas extends Component {
